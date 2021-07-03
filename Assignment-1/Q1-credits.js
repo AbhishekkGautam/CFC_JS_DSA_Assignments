0c93d0b96faf3c956eb65013f0ced79b05c787fa
Abhishek Gautam <abhishekgautm243@gmail.com>
2021-07-03 23:38:53 +0530

    Q1-credits

---
+++ Assignment-1/Q1-credits.js
@@ -0,0 +1,13 @@
+let readlineSync = require("readline-sync");
+
+let credits = readlineSync.question("Enter your credit scrore? ");
+
+if (credits >= 7500) {
+  console.log("Congrats! You earned TERA LEADER badge.");
+} else if (credits >= 6000) {
+  console.log("Congrats! You earned GEGA LEADER badge.");
+} else if (credits >= 4500) {
+  console.log("Congrats! You earned MEGA LEADER badge, Keep Shining.");
+} else {
+  console.log("Congrats! You earned RISING STAR badge, Keep Rising.");
+}
