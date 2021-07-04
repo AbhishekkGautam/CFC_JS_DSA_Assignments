9a6fac6b25c66f6b198570c71f04d172682e0eb6
Abhishek Gautam <abhishekgautm243@gmail.com>
2021-07-04 19:04:28 +0530

    Update Q6-reverse.js

--- Assignment-1/Q6-reverse.js
+++ Assignment-1/Q6-reverse.js
@@ -2,13 +2,12 @@ let readlineSync = require("readline-sync");
 
 let input = readlineSync.question("Enter your number - ");
 
-let a,
-  reverseNum = 0;
+let reverseNum = 0;
 
 num = input;
 
 while (num > 0) {
-  a = num % 10;
+  let a = num % 10;
   // num = parseInt(num / 10);
   // OR
   num = (num - (num % 10)) / 10;
